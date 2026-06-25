9c27df82c0f67fdc1971e9fc5b2d1fe7ea588ca0
Ortan777 <106433047+Ortan777@users.noreply.github.com>
2026-06-25 16:52:38 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,26 @@
+#print("Hello World","world",sep="#")
+#print("Hello World",end="")
+#print("Hello World")
+#x="10.5"
+#y="s"
+#print("abc">"abd")
+#x=input()
+#x=int(input())
+#print(int(float(x)))
+#print("\test\ntesting")
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print("""hello
+ #     my name is
+  #    John
+   #   """)
+#x="hello"
+#print(x[0:3:2]) #start:end:step
+#print(x[::-1]) #reverse string
+
+for i in range(1,10):
+    if i%2==0:
+        print(i)
+
+for i in range(0,10,2):
+    print(i)
